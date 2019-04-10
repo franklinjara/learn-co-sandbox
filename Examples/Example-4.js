180e77c31d0a30edc5cd629076af3e6c2cb410b2
Franklin <makyfj@hotmail.com>
2019-04-10 17:20:09 +0000

    Automatically backed up by Learn

---
+++ Examples/Example-4.js
@@ -0,0 +1,10 @@
+var dog = "thirsty"
+
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+} else if (dog === "thirsty")
+{
+  console.log("Refilling water bowl.")
+} else {
+  console.log("Reading newspaper.")
+}
---
+++ Examples/else-statements.js
@@ -0,0 +1,5 @@
+if (false) {
+  console.log("This will never get printed because the above statement evaluates to false.")
+} else {
+  console.log("This will get printed!")
+}
